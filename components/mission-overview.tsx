c25584d16d2692f5c3fed0d65429b371bd8f59cb
MainakVerse <mainakchaudhuri671@gmail.com>
2025-11-24 09:52:35 +0530

    Made video continuous

--- components/mission-overview.tsx
+++ components/mission-overview.tsx
@@ -53,14 +53,13 @@ export function MissionOverview() {
 
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/vU6PDFdgf8k?autoplay=1&mute=1&si=T8AkEWZ-NrmTzEZa"
+                src="https://www.youtube.com/embed/vU6PDFdgf8k?autoplay=1&mute=0&loop=1&playlist=vU6PDFdgf8k"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
               ></iframe>
-
             </div>
           </div>
 
